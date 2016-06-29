222b000bd982eb06b037d5093c11d18e10aa76d3
Patrick Wood <pfmwood@gmail.com>
2016-06-29 14:16:33 +1200

    added more gpio pins

--- Gpio/Enums.cs
+++ Gpio/Enums.cs
@@ -2,7 +2,32 @@
 {
     public enum GpioPinNumber
     {
-        Gpio11 = 11
+        Gpio02 = 2,
+        Gpio03,
+        Gpio04,
+        Gpio05,
+        Gpio06,
+        Gpio07,
+        Gpio08,
+        Gpio09,
+        Gpio10,
+        Gpio11,
+        Gpio12,
+        Gpio13,
+        Gpio14,
+        Gpio15,
+        Gpio16,
+        Gpio17,
+        Gpio18,
+        Gpio19,
+        Gpio20,
+        Gpio21,
+        Gpio22,
+        Gpio23,
+        Gpio24,
+        Gpio25,
+        Gpio26,
+        Gpio27,
     }
 
     public enum Direction
